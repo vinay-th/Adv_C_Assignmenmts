a04aa091ca1733cd98853a6c2a22629ad29de5ff
Vinay Thakor <144613261+vinay-th@users.noreply.github.com>
2024-03-31 23:14:54 +0530

    Update 13_rev_array.c

--- task 4/13_rev_array.c
+++ task 4/13_rev_array.c
@@ -1,49 +1,19 @@
 #include <stdio.h>
-void revAry(int *pt)
+
+void revArray(int* ptr, int count)
 {
-    int i, a[5];
-    int start = 0, end = 4, temp;
-    for (i = 0; i < 5; i++)
+    for (int i = count - 1 ; i >= 0; i--)
     {
-        printf("Enter the value of A[%d]: ",i);
-        scanf("%d", pt);
-        pt++;
-    }
-    pt -= 5;
-    printf("\nThe original array is------------\n\n");
-
-    for ( i = 0; i < 5; i++)
-    {
-        printf("A[%d] : %d\n", i, *pt);
-        pt++;
-    }
-    pt -= 5;
-
-    for ( i = 0; i < 5; i++)
-    {
-        a[i] = *pt;
-        pt++;
+        printf("%d \n", *(ptr + i));
     }
     
-    while (start < end) {
-        temp = a[start];
-        a[start] = a[end];
-        a[end] = temp;
-
-        start++;
-        end--;
-    }
-    printf("\nThe reversed array is------------\n\n");
-    for ( i = 0; i < 5; i++)
-    {
-        printf("A[%d] : %d\n", i, a[i]);
-    }
 }
+
 int main()
 {
-    int a[5];
-
-    revAry(a);   
+    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    revArray(a, 10);
     
     printf("\n");
     return 0;
